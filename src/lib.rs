b7205b430e75a676e45318d89d1470d7338569fe
Tom Webber <t.c.w.webber@gmail.com>
2025-08-05 22:37:48 +0200

    add clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,49 @@
-fn push_lower(output: &mut String, ch: char) {
-    for c in ch.to_lowercase() {
-        output.push(c);
-    }
-}
-fn push_upper(output: &mut String, ch: char) {
-    for c in ch.to_uppercase() {
-        output.push(c);
-    }
-}
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    warnings,
+    deprecated_safe,
+    future_incompatible,
+    keyword_idents,
+    let_underscore,
+    nonstandard_style,
+    refining_impl_trait,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    rust_2024_compatibility,
+    unused,
+    clippy::all,
+    clippy::pedantic,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::restriction,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![expect(clippy::doc_include_without_cfg, reason = "see issue #13918")]
+#![expect(clippy::blanket_clippy_restriction_lints, reason = "I want them all")]
+#![allow(
+    clippy::implicit_return,
+    clippy::pattern_type_mismatch,
+    clippy::missing_inline_in_public_items,
+    reason = "bad lint"
+)]
+#![expect(clippy::unreachable, reason = "I trust")]
 
+/// Type of the read character, which will determine how it is processed
 #[derive(Debug)]
 enum Type {
-    Lowercase,
+    /// The character is an uppercase letter or a number.
     CapitalOrNumber,
-    Symbol,
+    /// The character is a lowercase letter.
+    Lowercase,
+    /// We are reading before the first character of the string.
     None,
+    /// The character is a symbol.
+    Symbol,
 }
 
 impl From<char> for Type {
@@ -29,6 +58,123 @@ impl From<char> for Type {
     }
 }
 
+/// Converts the string to various cases.
+pub trait Recase {
+    /// Converts the string to `camelCase`.
+    fn to_camel_case(&self) -> String;
+    /// Converts the string to `Capitalised Case`.
+    fn to_capitalised_case(&self) -> String;
+    /// Converts the string to `CONSTANT_CASE`.
+    fn to_constant_case(&self) -> String;
+    /// Converts the string to `dot.case`.
+    fn to_dot_case(&self) -> String;
+    /// Converts the string to `kebab-case`.
+    fn to_kebab_case(&self) -> String;
+    /// Converts the string to `PascalCase`.
+    fn to_pascal_case(&self) -> String;
+    /// Converts the string to `Sentence case`.
+    fn to_sentence_case(&self) -> String;
+    /// Converts the string to `snake_case`.
+    fn to_snake_case(&self) -> String;
+}
+
+impl Recase for str {
+    fn to_camel_case(&self) -> String {
+        to_new_case(self, false, push_upper, push_lower)
+    }
+
+    fn to_capitalised_case(&self) -> String {
+        to_new_case(
+            self,
+            true,
+            |output, ch| {
+                output.push(' ');
+                push_upper(output, ch);
+            },
+            push_lower,
+        )
+    }
+
+    fn to_constant_case(&self) -> String {
+        to_new_case(
+            self,
+            true,
+            |output, ch| {
+                output.push('_');
+                push_upper(output, ch);
+            },
+            push_upper,
+        )
+    }
+
+    fn to_dot_case(&self) -> String {
+        to_new_case(
+            self,
+            false,
+            |output, ch| {
+                output.push('.');
+                push_lower(output, ch);
+            },
+            push_lower,
+        )
+    }
+
+    fn to_kebab_case(&self) -> String {
+        to_new_case(
+            self,
+            false,
+            |output, ch| {
+                output.push('-');
+                push_lower(output, ch);
+            },
+            push_lower,
+        )
+    }
+
+    fn to_pascal_case(&self) -> String {
+        to_new_case(self, true, push_upper, push_lower)
+    }
+
+    fn to_sentence_case(&self) -> String {
+        to_new_case(
+            self,
+            true,
+            |output, ch| {
+                output.push(' ');
+                push_lower(output, ch);
+            },
+            push_lower,
+        )
+    }
+
+    fn to_snake_case(&self) -> String {
+        to_new_case(
+            self,
+            false,
+            |output, ch| {
+                output.push('_');
+                push_lower(output, ch);
+            },
+            push_lower,
+        )
+    }
+}
+
+/// Pushes the lowercase representation of the given character to the output string.
+fn push_lower(output: &mut String, ch: char) {
+    for byte in ch.to_lowercase() {
+        output.push(byte);
+    }
+}
+
+/// Pushes the uppercase representation of the given character to the output string.
+fn push_upper(output: &mut String, ch: char) {
+    for byte in ch.to_uppercase() {
+        output.push(byte);
+    }
+}
+
+/// Determines if a character should be interpreted as a word beginning or not, and handles the push.
 fn handle_func<F: Fn(&mut String, char), G: Fn(&mut String, char)>(
     output: &mut String,
     old: &Type,
@@ -38,29 +184,32 @@ fn handle_func<F: Fn(&mut String, char), G: Fn(&mut String, char)>(
     prev: char,
     next_is_lowercase: bool,
 ) -> Type {
-    let new = Type::from(dbg!(prev));
-    match dbg!((&old, &new)) {
+    let new = Type::from(prev);
+    match (&old, &new) {
         (Type::None, _) => {
             if is_first_capitalised {
-                push_upper(output, prev)
+                push_upper(output, prev);
             } else {
-                push_lower(output, prev)
+                push_lower(output, prev);
             }
         }
         (_, Type::None) => unreachable!(),
         (_, Type::Symbol) => (),
         (Type::Symbol, _) | (Type::Lowercase, Type::CapitalOrNumber) => on_word_begin(output, prev),
         (Type::CapitalOrNumber, Type::CapitalOrNumber) if next_is_lowercase => {
-            on_word_begin(output, prev)
+            on_word_begin(output, prev);
         }
         (Type::Lowercase, Type::Lowercase)
         | (Type::CapitalOrNumber, Type::CapitalOrNumber | Type::Lowercase) => {
-            on_normal(output, prev)
+            on_normal(output, prev);
         }
     }
     new
 }
 
+/// Converts a string to a new case based on the provided functions for word beginnings and normal characters.
+///
+/// This is a wrapper function that handles the conversion logic for all the casing methods.
 fn to_new_case<F: Fn(&mut String, char), G: Fn(&mut String, char)>(
     value: &str,
     is_first_capitalised: bool,
@@ -97,97 +246,4 @@ fn to_new_case<F: Fn(&mut String, char), G: Fn(&mut String, char)>(
     }
 
     output
-}
-
-pub trait Recase {
-    fn to_camel_case(&self) -> String;
-    fn to_pascal_case(&self) -> String;
-    fn to_snake_case(&self) -> String;
-    fn to_kebab_case(&self) -> String;
-    fn to_sentence_case(&self) -> String;
-    fn to_capitalised_case(&self) -> String;
-    fn to_dot_case(&self) -> String;
-    fn to_constant_case(&self) -> String;
-}
-
-impl Recase for str {
-    fn to_camel_case(&self) -> String {
-        to_new_case(self, false, push_upper, push_lower)
-    }
-
-    fn to_pascal_case(&self) -> String {
-        to_new_case(self, true, push_upper, push_lower)
-    }
-
-    fn to_snake_case(&self) -> String {
-        to_new_case(
-            self,
-            false,
-            |output, ch| {
-                output.push('_');
-                push_lower(output, ch);
-            },
-            push_lower,
-        )
-    }
-
-    fn to_kebab_case(&self) -> String {
-        to_new_case(
-            self,
-            false,
-            |output, ch| {
-                output.push('-');
-                push_lower(output, ch);
-            },
-            push_lower,
-        )
-    }
-
-    fn to_sentence_case(&self) -> String {
-        to_new_case(
-            self,
-            true,
-            |output, ch| {
-                output.push(' ');
-                push_lower(output, ch);
-            },
-            push_lower,
-        )
-    }
-
-    fn to_capitalised_case(&self) -> String {
-        to_new_case(
-            self,
-            true,
-            |output, ch| {
-                output.push(' ');
-                push_upper(output, ch);
-            },
-            push_lower,
-        )
-    }
-
-    fn to_dot_case(&self) -> String {
-        to_new_case(
-            self,
-            false,
-            |output, ch| {
-                output.push('.');
-                push_lower(output, ch);
-            },
-            push_lower,
-        )
-    }
-
-    fn to_constant_case(&self) -> String {
-        to_new_case(
-            self,
-            true,
-            |output, ch| {
-                output.push('_');
-                push_upper(output, ch);
-            },
-            push_upper,
-        )
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    warnings,
+    deprecated_safe,
+    future_incompatible,
+    keyword_idents,
+    let_underscore,
+    nonstandard_style,
+    refining_impl_trait,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    rust_2024_compatibility,
+    unused,
+    clippy::all,
+    clippy::pedantic,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::restriction,
+    clippy::nursery,
+    clippy::cargo
+)]
+#![expect(clippy::doc_include_without_cfg, reason = "see issue #13918")]
+#![expect(clippy::blanket_clippy_restriction_lints, reason = "I want them all")]
+#![allow(
+    clippy::implicit_return,
+    clippy::pattern_type_mismatch,
+    clippy::missing_inline_in_public_items,
+    clippy::question_mark_used,
+    reason = "bad lint"
+)]
+
 use clap::{CommandFactory as _, Parser};
 use std::io::{self, BufRead as _};
 
@@ -5,36 +40,38 @@ use recase::Recase as _;
 
 /// Convert the case of the output. Choose one of the options below:
 #[derive(Parser, Debug)]
+#[expect(clippy::struct_excessive_bools, reason = "CLI")]
 struct Args {
-    /// If no value is provided, reads from stdin (e.g. for pipes).
-    value: Option<String>,
-    /// thisIsCamelCase
+    /// `thisIsCamelCase`
     #[arg(short, long)]
     camel: bool,
-    /// ThisIsPascalCase
-    #[arg(short, long)]
-    pascal: bool,
-    /// this_is_snake_case
-    #[arg(short, long)]
-    snake: bool,
-    /// THIS_IS_CONSTANT_CASE (or UPPER_CASE)
-    #[arg(short = 'u', long)]
-    constant: bool,
-    /// this-is-kebab-case (or dashed-case)
-    #[arg(short, long)]
-    kebab: bool,
-    /// This Is Capitalised Case
+    /// `This Is Capitalised Case`
     #[arg(short = 'a', long)]
     capitalised: bool,
-    /// This is sentence case
-    #[arg(short = 'e', long)]
-    sentence: bool,
-    /// this.is.dot.case
+    /// `THIS_IS_CONSTANT_CASE` (or `UPPER_CASE`)
+    #[arg(short = 'u', long)]
+    constant: bool,
+    /// `this.is.dot.case`
     #[arg(short, long)]
     dot: bool,
+    /// `this-is-kebab-case` (or `dashed-case`)
+    #[arg(short, long)]
+    kebab: bool,
+    /// `ThisIsPascalCase`
+    #[arg(short, long)]
+    pascal: bool,
+    /// `This is sentence case`
+    #[arg(short = 'e', long)]
+    sentence: bool,
+    /// `this_is_snake_case`
+    #[arg(short, long)]
+    snake: bool,
+    /// If no value is provided, reads from stdin (e.g. for pipes).
+    value: Option<String>,
 }
 
 impl Args {
+    /// Applies the selected case to the provided value.
     fn apply_case(&self, value: &str) -> String {
         if self.camel {
             value.to_camel_case()
@@ -57,6 +94,10 @@ impl Args {
         }
     }
 
+    /// Runs the command, processing input and applying the selected case.
+    ///
+    /// This function reads from stdin if no value is provided, or uses the provided value.
+    #[expect(clippy::print_stdout, reason = "this is a CLI")]
     fn run(&self) -> Result<(), io::Error> {
         let nb = [
             self.camel,
@@ -73,23 +114,26 @@ impl Args {
             panic("You must provide 1 and 1 only output case.")
         } else if nb == 0 {
             panic("No output case provided. Please choose the option for your output.")
-        }
-
-        if let Some(value) = &self.value {
-            println!("{}", self.apply_case(value));
         } else {
-            let stdin = io::stdin();
-            for line in stdin.lock().lines() {
-                println!("{}", self.apply_case(&line?));
+            if let Some(value) = &self.value {
+                println!("{}", self.apply_case(value));
+            } else {
+                let stdin = io::stdin();
+                for line in stdin.lock().lines() {
+                    println!("{}", self.apply_case(&line?));
+                }
             }
-        }
 
-        Ok(())
+            Ok(())
+        }
     }
 }
 
+/// Panics with a formatted error message and prints the help message.
+#[expect(unused_must_use, reason = "don't crash in panic")]
+#[expect(clippy::panic, reason = "reason of the function")]
 fn panic(msg: &str) -> ! {
-    Args::command().print_help().unwrap();
+    Args::command().print_help();
     panic!("\x1b[31m{msg}\x1b[0m");
 }
 
@@ -109,7 +153,7 @@ mod tests {
     }
 
     #[test]
-    fn test_args() {
+    fn cli_testing() {
         test(
             &["", "--camel", "this_is_snake_case"],
             "this_is_snake_case",
